d250504089b5e53bede7aeb96404b2503003b0a8
Brent Lemons <brent_lemons@yahoo.com>
2017-12-09 12:09:31 -0600

    init

---
+++ src/main/java/com/fltck/aws/agent/AwsSpotAgentApplication.java
@@ -0,0 +1,12 @@
+package com.fltck.aws.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpotAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpotAgentApplication.class, args);
+	}
+}
